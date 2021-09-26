80081199c3708974b4b30fd03d25bc4d7b9a1c8f
Nirman <nkesari@outlook.com>
2021-09-26 16:56:25 +1000

    notes CRUD complete

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,13 +1,83 @@
-import React from 'react'
-import notes from '../assets/data'
+import React, {useState, useEffect} from 'react'
+// import notes from '../assets/data'
 import { Link } from 'react-router-dom'
 import { ReactComponent as ArrowLeft} from '../assets/arrow-left.svg'
 
-const NotePage = ({match}) => {
+
+const NotePage = ({match, history}) => {
 
     let noteId = match.params.id
-    let note = notes.find(note => note.id === Number(noteId))
 
+
+    // let note = notes.find(note => note.id === Number(noteId))
+    let [note, setNote] = useState(null)
+
+    useEffect(() => {
+        getNote()
+        return () => {
+            
+        }
+    }, [noteId])
+
+
+    let getNote = async () => {
+        if (noteId === 'new') return
+        let response = await fetch(`http://localhost:8000/notes/${noteId}`)
+        let data = await response.json()
+        setNote(data)
+    }
+
+    let createNote = async() => {
+        await fetch(`http://localhost:8000/notes/`, {
+            method:'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({...note, 'updated': new Date() })
+
+        })
+    }
+
+
+    let updateNote = async() => {
+                await fetch(`http://localhost:8000/notes/${noteId}`, {
+                    method:'PUT',
+                    headers: {
+                        'Content-Type':'application/json'
+                    },
+                    body: JSON.stringify({...note, 'updated': new Date() })
+
+                })
+            }
+
+
+    let deleteNote = async () =>{
+            await fetch(`http://localhost:8000/notes/${noteId}`, {
+                method:'DELETE',
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify(note)
+
+            })
+
+            history.push('/')
+
+    }
+
+
+    let handleSubmit = () => {
+        
+        if(noteId !== 'new' && !note.body){
+            deleteNote()
+        } else if (noteId !== 'new'){
+            updateNote()
+
+        } else if (noteId == 'new' && note!== null){
+            createNote()
+        }
+        history.push('/')
+    }
 
 
 
@@ -16,13 +86,22 @@ const NotePage = ({match}) => {
             <div className="note-header">
                 <h3>
                     <Link to="/">
-                        <ArrowLeft />
+                        <ArrowLeft onClick={ handleSubmit} />
                     </Link>
 
                 </h3>
+                { noteId !== 'new' ? 
+                (
+                    <button onClick={deleteNote}>Delete</button>
+                ):(  
+                    <button onClick={handleSubmit}>Done</button> 
+                  )
+                
+                }
+                
             </div>
             
-            <textarea value={note?.body}>  
+            <textarea value={note?.body} onChange={(e) => { setNote({ ...note, 'body':e.target.value}) }} value={note?.body} >  
 
             </textarea>
             {/* '?' used above is used to not throw error if body is not available of a particular note. For examle /21 is not in dataset so it throws error. '?' removes error and reponds with empty body.  */ }      
--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -1,23 +1,50 @@
-import React from 'react'
-import notes from '../assets/data'
+import React, { useState, useEffect } from 'react'
+// import notes from '../assets/data'
 import ListItem from '../componenets/ListItem'
+import AddButton from '../componenets/AddButton'
 
 const NotesListPage = () => {
+
+    let [notes, setNotes] = useState([])
+
+
+    useEffect(() => {
+        getNotes()
+        return () => {
+            
+        }
+    },[]) 
+    // Empty [] as we want to mount componenet only once. 
+
+
+    let getNotes = async () => {
+
+            
+
+            let response = await fetch('http://localhost:8000/notes/')
+            let data = await response.json()
+            // console.log("data", data)
+            setNotes(data)
+    }
+
     return (
         <div className="notes">
 
             <div className="notes-header">
                 <h2 className="notes-title">&#9782; Notes</h2>
-                <p clasName="notes-count"> {notes.length} </p>
+                <p clasName="notes-count"> {notes?.length} </p>
             </div>
 
             <div className="notes-list">
                 {notes.map((note, index) => 
 
-                    <ListItem key={index} note={note} body={note.body} />
+                    <ListItem key={index} note={note} body={note?.body} />
 
                 )}
-            </div> 
+            </div>
+
+            <AddButton />
+
         </div>
     )
 }
